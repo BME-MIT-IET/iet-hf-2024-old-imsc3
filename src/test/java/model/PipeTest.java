bce01f014e539c61c4e26c91f488ee5874b486c2
Tényi Tamás <tenyi.tamas28@gmail.com>
2024-05-19 16:10:33 +0200

    Add further test cases to Pipe

--- src/test/java/model/PipeTest.java
+++ src/test/java/model/PipeTest.java
@@ -1,13 +1,15 @@
 package model;
 
+import Model.IO_Manager;
 import Model.Pipe;
 import Model.PointCounter;
+import Model.WaterNode;
 import junit.framework.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.*;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
@@ -18,21 +20,57 @@ public class PipeTest {
     private PointCounter pointCounter;
 
     @BeforeEach
-    public void init(){
+    public void init() {
         pointCounter = mock(PointCounter.class);
         this.pipe = new Pipe(pointCounter);
     }
+
     @Test
     public void constructorTest() {
         Assert.assertNotNull(pipe);
     }
 
     @Test
-    public void gainWaterTest() {
-        var res = pipe.GainWater(10);
-
-        verify(pointCounter, never()).AddSaboteurPoints(anyInt());
-        assertEquals(1, res);
-
+    public void gainWaterTestWhenPipeIsFree() {
+        pipe.GainWater(10);
+        verifyNoInteractions(pointCounter);
     }
+
+    @Test
+    public void gainWaterTestWhenPipeIsBroken() {
+        pipe.setBroken(true);
+        pipe.GainWater(10);
+
+        verify(pointCounter).AddSaboteurPoints(10);
+    }
+
+    @Test
+    public void loseWaterTest() {
+        pipe.setHeldWater(20);
+        pipe.LoseWater(12);
+
+        assertEquals(8, pipe.getHeldWater());
+
+        pipe.LoseWater(10);
+
+        assertEquals(0, pipe.getHeldWater());
+    }
+
+    @Test
+    public void AddWaterNodeShouldOnlySucceedIfPipeHasFreeNode() {
+        assertTrue(pipe.AddWaterNode(mock(WaterNode.class)));
+        assertTrue(pipe.AddWaterNode(mock(WaterNode.class)));
+        assertFalse(pipe.AddWaterNode(mock(WaterNode.class)));
+    }
+
+    @Test
+    public void repairedTestWhenBroken() {
+        pipe.setBroken(true);
+
+        pipe.Repaired();
+
+        assertFalse(pipe.isBroken());
+    }
+
+
 }
